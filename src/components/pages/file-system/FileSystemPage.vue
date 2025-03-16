bcab17681a13d53786db0d153951bc13430359b7
KuroNeko359 <jz2077056966@gmail.com>
2025-03-16 23:29:57 +0800

    Delete some redundant code

--- src/components/pages/file-system/FileSystemPage.vue
+++ src/components/pages/file-system/FileSystemPage.vue
@@ -41,39 +41,9 @@ const refreshPanel = () => {
 </script>
 
 <template>
-  <!--  Page -> Panel -> -->
-
   <card :title="t('file-system')" class="w-full">
     <FileSystemPanel :refreshFunction="refreshPanel" :key="refreshKey" :path="filePath"></FileSystemPanel>
-
   </card>
-
-  <!--  <modal name="upload-file" title="上传文件">-->
-  <!--    <br>-->
-  <!--    <label class="input w-full">-->
-  <!--      Path-->
-  <!--      <input type="text" class="grow" placeholder="src/app/"/>-->
-  <!--      <span class="badge badge-neutral badge-xs">Optional</span>-->
-  <!--    </label>-->
-  <!--    <fieldset class="fieldset">-->
-  <!--      <input type="file" class="file-input w-full" @change="upload.handleFileChange"/>-->
-  <!--    </fieldset>-->
-  <!--    <primary-button @click="upload.uploadFile" title="Upload"></primary-button>-->
-  <!--  </modal>-->
-  <!--  &lt;!&ndash; The button to open modal &ndash;&gt;-->
-  <!--  <label for="my_modal_6" class="btn">open modal</label>-->
-
-  <!--  &lt;!&ndash; Put this part before </body> tag &ndash;&gt;-->
-  <!--  <input type="checkbox" id="my_modal_6" class="modal-toggle" />-->
-  <!--  <div class="modal" role="dialog">-->
-  <!--    <div class="modal-box">-->
-  <!--      <h3 class="text-lg font-bold">Hello!</h3>-->
-  <!--      <p class="py-4">This modal works with a hidden checkbox!</p>-->
-  <!--      <div class="modal-action">-->
-  <!--        <label for="my_modal_6" class="btn">Close!</label>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
 </template>
 
 <style scoped>
