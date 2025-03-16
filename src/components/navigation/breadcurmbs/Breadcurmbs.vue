cd31c4316ca16609acff262b143d906fe8bc6066
KuroNeko359 <jz2077056966@gmail.com>
2025-03-16 23:30:07 +0800

    Delete some redundant code

--- src/components/navigation/breadcurmbs/Breadcurmbs.vue
+++ src/components/navigation/breadcurmbs/Breadcurmbs.vue
@@ -32,30 +32,10 @@ let filePathList = pathStringToList(props.path);
           v-for="(_,index) in filePathList"
       ></breadcurmbs-node>
 
-
-      <!--      <li>-->
-      <!--      <span class="inline-flex items-center gap-2">-->
-      <!--        <svg-->
-      <!--            xmlns="http://www.w3.org/2000/svg"-->
-      <!--            fill="none"-->
-      <!--            viewBox="0 0 24 24"-->
-      <!--            class="h-4 w-4 stroke-current">-->
-      <!--          <path-->
-      <!--              stroke-linecap="round"-->
-      <!--              stroke-linejoin="round"-->
-      <!--              stroke-width="2"-->
-      <!--              d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>-->
-      <!--        </svg>-->
-      <!--        Add Document-->
-      <!--      </span>-->
-      <!--      </li>-->
     </ul>
   </div>
 </template>
 
 <style scoped>
-.test {
-  border: 1px solid black;
-  overflow: auto; /* 超出时显示滚动条 */
-}
+
 </style>
