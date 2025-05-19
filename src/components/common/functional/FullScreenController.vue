a2354b8f61b688702586df0fbe17fbba42c43277
KuroNeko359 <jz2077056966@gmail.com>
2025-05-19 09:39:08 +0800

    changed position

--- src/components/common/functional/FullScreenController.vue
+++ src/components/common/functional/FullScreenController.vue
@@ -16,7 +16,17 @@ const toggleFullScreen = () => {
   }
   toggleScreenStatus();
 };
-
+/**
+ * 切换全屏状态的函数。
+ * 切换 isFullScreen 的值，
+ * 并将新的全屏状态信息保存到 localStorage 中。
+ */
+const toggleScreenStatus = () => {
+  isFullScreen.value = !isFullScreen.value;
+  const newStatus = isFullScreen.value ? 'true' : 'false';
+  localStorage.setItem('is-full-screen', newStatus);
+  console.log(`FullScreen status changed to: ${newStatus}`);
+};
 /**
  * 生命周期钩子，在组件挂载后执行。
  * 调用 setFullScreenStatus 函数来从保存的配置中切换网站的显示状态（全屏/非全屏）。
